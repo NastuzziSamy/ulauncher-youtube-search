a04d2f3d129625928fea26d747edb023c4e315b7
NastuzziSamy <samy@nastuzzi.fr>
2021-03-27 11:43:53 +0100

    Use full contents result for items

--- src/youtube_search.py
+++ src/youtube_search.py
@@ -28,35 +28,39 @@ class YoutubeSearch():
         response = requests.get(SEARCH_BASE_URL + urllib.parse.quote(self.query)).text
         results = []
         start = (
-            response.index("ytInitialData") + len("ytInitialData") + 3
+            response.index('ytInitialData') + len('ytInitialData') + 3
         )
-        end = response.index("};", start) + 1
+        end = response.index('};', start) + 1
 
         data = json.loads(response[start:end])
 
-        videos = data["contents"]["twoColumnSearchResultsRenderer"]["primaryContents"] \
-            ["sectionListRenderer"]["contents"][0]["itemSectionRenderer"]["contents"]
+        contents = data['contents']['twoColumnSearchResultsRenderer']['primaryContents']['sectionListRenderer']['contents']
 
-        for video in videos[:MAX_RESULTS]:
-            res = {}
-            if "videoRenderer" in video.keys():
-                video_data = video.get("videoRenderer", {})
-                video_id = video_data.get("videoId", None)
-                thumbnail = video_data.get("thumbnail", {}).get("thumbnails", [{}])[0].get("url", None)
-                thumbnail_path = None
+        for content in contents:
+            items = content.get('itemSectionRenderer', {}).get('contents', {})
 
-                if self.show_thumbnails and thumbnail:
-                    thumbnail_path = save_thumbnail(thumbnail, video_id)
+            for item in items:
+                if len(results) > MAX_RESULTS:
+                    return results
 
-                results.append({
-                    'id': video_id,
-                    'thumbnail': thumbnail_path,
-                    'title': video_data.get("title", {}).get("runs", [{}])[0].get("text", '').replace('&', 'and'),
-                    'channel': video_data.get("longBylineText", {}).get("runs", [{}])[0].get("text", None),
-                    'date': video_data.get("publishedTimeText", {}).get("simpleText"),
-                    'duration': video_data.get("lengthText", {}).get("simpleText", 0),
-                    'views': video_data.get("viewCountText", {}).get("simpleText", 0),
-                    'url': BASE_URL + video_data.get("navigationEndpoint", {}).get("commandMetadata", {}).get("webCommandMetadata", {}).get("url", None)
-                })
+                if 'videoRenderer' in item.keys():
+                    video_data = item['videoRenderer']
+                    video_id = video_data.get('videoId', None)
+                    thumbnail = video_data.get('thumbnail', {}).get('thumbnails', [{}])[0].get('url', None)
+                    thumbnail_path = None
+
+                    if self.show_thumbnails and thumbnail:
+                        thumbnail_path = save_thumbnail(thumbnail, video_id)
+
+                    results.append({
+                        'id': video_id,
+                        'thumbnail': thumbnail_path,
+                        'title': video_data.get('title', {}).get('runs', [{}])[0].get('text', '').replace('&', 'and'),
+                        'channel': video_data.get('longBylineText', {}).get('runs', [{}])[0].get('text', None),
+                        'date': video_data.get('publishedTimeText', {}).get('simpleText'),
+                        'duration': video_data.get('lengthText', {}).get('simpleText', 0),
+                        'views': video_data.get('viewCountText', {}).get('simpleText', 0),
+                        'url': BASE_URL + video_data.get('navigationEndpoint', {}).get('commandMetadata', {}).get('webCommandMetadata', {}).get('url', None)
+                    })
 
         return results
